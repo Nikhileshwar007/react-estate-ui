55fa17c73292c2b98984a61987ca93f23405e0c1
Nikhileshwar007 <nikhilg937@gmail.com>
2024-06-22 11:56:09 +0530

    added navbar.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import Navbar from "./components/Navbar/Navbar"
+import "./layout.scss"
+import  HomePage from "./routes/homePage/homePage"
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="layout">
+      <div className="navbar">
+      <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+    </div>
   )
 }
 
