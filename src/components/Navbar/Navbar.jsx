b2f04498f19cdd8e184079672df7c88f5d991a66
Nikhileshwar007 <nikhilg937@gmail.com>
2024-06-28 20:13:09 +0530

    modified navbar jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
+
 function Navbar() {
   const [open, setOpen] = useState(false);
 
-  const user = true;
+  const user = false;
   return (
     <nav>
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
-          <span>LamaEstate</span>
+          <span>Nikhil Estate</span>
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>
@@ -17,8 +19,26 @@ function Navbar() {
         <a href="/">Agents</a>
       </div>
       <div className="right">
+        {user ? (
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <span>Nikhil</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
             <a href="/">Sign in</a>
-            <a href="/" className="register">Sign up</a>
+            <a href="/" className="register">
+              Sign up
+            </a>
+          </>
+        )}
         <div className="menuIcon">
           <img
             src="/menu.png"
